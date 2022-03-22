c59e29983dfd30d8c668a190294a5f614e083190
nemelkiy <nemelkiy@gmail.com>
2022-03-23 00:31:11 +0300

    Start front work

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Добро пожаловать в Mask Detector от группы разработчиков МБД2131 МТУСИ</title>
+
+    <link rel="stylesheet" href="front/css/main.css">
+</head>
+<body>
+    <div class="header-line">
+        <div class="container">
+        <div class="logo">
+            
+        </div>
+        <nav>
+            <a href="">Главная</a>
+            <a href="">Цель приложения</a>
+            <a href="">Загрузить фаил</a>
+            <a href="">Последние результаты</a>
+        </nav>
+        </div>
+    </div>
+</body>
+</html>
