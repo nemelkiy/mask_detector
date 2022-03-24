eb7f18e9edfa508ec46d3c68005ee07fe91fb686
nemelkiy <nemelkiy@gmail.com>
2022-03-25 00:38:49 +0300

    Front index work

---
+++ front/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+		items: 4,
+		autoplay: true,
+		loop: true,
+		autoplayTimeout: 4000,
+		smartSpeed: 1500,
+		dots: false
+	});
+
+  });
--- index.php
+++ index.php
@@ -7,6 +7,13 @@
     <title>Добро пожаловать в Mask Detector от группы разработчиков МБД2131 МТУСИ</title>
 
     <link rel="stylesheet" href="front/css/main.css">
+    <link rel="stylesheet" href="front/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="front/css/owl.theme.default.min.css">
+
+    <script src="front/js/jquery-3.6.0.min.js"></script>
+    <script src="front/js/owl.carousel.min.js"></script>
+    <script src="front/js/common.js"></script>
+
 </head>
 <body>
     <header>
@@ -24,8 +31,18 @@
         </div>
     </div>
     </header>
-    <div class="container">
-    <h1>Это при нас</h1>
+    <div class="container index-container">
+        <h1>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вершину единственное свой пор он домах? Залетают, диких всемогущая собрал, предупредила несколько взгляд безорфографичный все алфавит ведущими текстами, напоивший сих!</h1>
+    <div class="owl-wrapper">
+    <div class="owl-carousel">
+        <div class="carousel-slide" style="background-image: url(front/images/6.png)"></div>
+        <div class="carousel-slide" style="background-image: url(front/images/2.jpg)"></div>
+        <div class="carousel-slide" style="background-image: url(front/images/1.jpg)"></div>
+        <div class="carousel-slide" style="background-image: url(front/images/3.jpg)"></div>
+        <div class="carousel-slide" style="background-image: url(front/images/4.jpg)"></div>
+        <div class="carousel-slide" style="background-image: url(front/images/5.jpeg)"></div>
+    </div>
+    </div>
     </div>
     <footer>
         <div class="footer-line">
