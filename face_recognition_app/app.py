dc3b6ff47d91e20b2c5a906fa08dda4309d84da5
CookIsGood <Morkovo4ka513@yandex.ru>
2022-04-06 14:27:52 +0300

    Испралены параметры запуска

--- face_recognition_app/app.py
+++ face_recognition_app/app.py
@@ -1,9 +1,13 @@
 from buisness_logic import Recognizer
+import pika
 
 
 def main():
-
-    rec = Recognizer("/app/model/my_model.h5")
+    params = pika.URLParameters('amqp://admin:Admin1234@rabbitmq-1:5672/%2F')
+    rec = Recognizer(path_to_model="/app/model/my_model.h5",
+                     params=params,
+                     queue="default_queue",
+                     queue_pub="default_queue_pub")
     rec.receive_msg()
 
 
