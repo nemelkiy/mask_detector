9dc137674edd05534fc06c82e2872057ebe41e72
CookIsGood <Morkovo4ka513@yandex.ru>
2022-05-14 17:24:46 +0300

    Реализована логика пропуска кадров где нет нарушителей

--- face_recognition_app/app.py
+++ face_recognition_app/app.py
@@ -6,8 +6,8 @@ def main():
     params = pika.URLParameters('amqp://admin:Admin1234@rabbitmq-1:5672/%2F')
     rec = Recognizer(path_to_model="/app/model/my_model.h5",
                      params=params,
-                     queue="default_queue",
-                     queue_pub="default_queue_pub")
+                     queue="shots_for_processing",
+                     queue_pub="result_shots")
     rec.receive_msg()
 
 
